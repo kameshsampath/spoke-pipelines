4077b6f7c3b26874da9a4e94efc000d62ec53525
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-05-30 22:25:12 +0530

    WIP-05302019-2230

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ set -o pipefail
 _CURR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 docker run -t -u `id -u` -v ${_CURR_DIR}/artifacts:/runner/artifacts:Z \
- -v ${_CURR_DIR}/out:/runner/out:Z \
+ -v ${_CURR_DIR}/project/out:/runner/out:Z \
  -v ${_CURR_DIR}/env:/runner/env:Z \
  -v ${_CURR_DIR}/inventory:/runner/inventory:Z \
  -v ${_CURR_DIR}/project:/runner/project:Z \

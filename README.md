a41ba65b1f893c6d605c30f4e22fdf2442cd709a
Kamesh Sampath <kamesh.sampath@gmail.com>
2020-05-31 15:55:07 +0530

    WIP - ansible runner build

--- README.md
+++
@@ -1,2 +0,0 @@
-# Spoke Pipelines
-The Pipeline for Deploying ACM Spokes using Tektoncd
--- run.sh
+++ run.sh
@@ -6,11 +6,19 @@ set -o pipefail
 
 _CURR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
-docker run -t -u `id -u` -v ${_CURR_DIR}/artifacts:/runner/artifacts:Z \
+if [ ! -f ${_CURR_DIR}/project/out/.kubeconfig ];
+then
+  echo "$(kind get kubeconfig)" \
+    | tee ${_CURR_DIR}/project/out/.kubeconfig
+fi
+
+docker run -t  \
+ -v ${_CURR_DIR}/project/artifacts:/runner/artifacts:Z \
  -v ${_CURR_DIR}/project/out:/runner/out:Z \
  -v ${_CURR_DIR}/env:/runner/env:Z \
  -v ${_CURR_DIR}/inventory:/runner/inventory:Z \
  -v ${_CURR_DIR}/project:/runner/project:Z \
  -e PROJECT_DIR=${_CURR_DIR}/project \
  -e RUNNER_PLAYBOOK=generate-spoke-resources.yaml \
- ansible/ansible-runner:latest
+ -e KUBECONFIG=/runner/project/out/.kubeconfig \
+ registry.redhat.io/ansible-tower-36/ansible-runner-rhel7
